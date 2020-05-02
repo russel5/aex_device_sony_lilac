6db21f3bc278f8f9583b47d8a338596b95dff0de
russel5 <rnuriyev82@gmail.com>
2020-05-02 22:54:38 +0500

    init

---
+++ lineage_lilac.mk
@@ -0,0 +1,40 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/sony/lilac/device.mk)
+
+# Product API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+### BOOTANIMATION
+# vendor/lineage/config/common_full_phone.mk
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+# vendor/lineage/config/common.mk
+TARGET_BOOTANIMATION_HALF_RES := true
+
+### LINEAGE
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_lilac
+PRODUCT_DEVICE := lilac
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := G8441
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=lilac \
+    PRIVATE_BUILD_DESC="G8441-user 9 47.2.A.11.228 3311891731 release-keys"
+
+BUILD_FINGERPRINT := Sony/G8441/G8441:9/47.2.A.11.228/3311891731:user/release-keys
+
+ifneq ($(LINEAGE_DEV_CERTIFICATE),)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
+endif
+
+ifneq ($(LINEAGE_VERITY_CERTIFICATE),)
+    PRODUCT_VERITY_SIGNING_KEY := $(LINEAGE_VERITY_CERTIFICATE)
+endif
---
+++ vendor_prop.mk
@@ -0,0 +1,7 @@
+# Disable HDR
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.hwc_disable_hdr=1
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.block_allow_data=1
